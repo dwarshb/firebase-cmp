7de9cf1be27fa8d4b67863b4403f092e4c4bc108
dwarshb <dwarsh.b@gmail.com>
2025-04-30 15:56:56 -0400

    Required files

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "chAItalk"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+include(":composeApp")
+include(":firebase")
